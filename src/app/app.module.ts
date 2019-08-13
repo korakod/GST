ac1ee6a9b233b4cb3eae0af8ba7e4978d0649985
korakod.s <korakod07@gmail.com>
2019-08-13 15:21:04 +0700

    init component drag&drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragAndDropComponent
   ],
   imports: [
     BrowserModule,
+    DragDropModule,
     AppRoutingModule
   ],
   providers: [],
